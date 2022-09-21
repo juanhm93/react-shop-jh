5d623f39fe6005bda36bc4e1643569e05df4c7f5
Juan M Hernandez <juanhm93@gmail.com>
2022-09-20 22:03:22 -0500

    add class course to class 18

---
+++ src/routes/App.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Layout from '../containers/Layout';
+import RecoveryPassword from '../pages/RecoveryPassword';
+import Login from '../pages/Login';
+import Home from '../pages/Home';
+import SendEmail from '../pages/SendEmail';
+import NewPassword from '../pages/NewPassword';
+import MyAccount from '../pages/MyAccount';
+import CreateAccount from '../pages/CreateAccount';
+import Checkout from '../pages/Checkout';
+import Orders from '../pages/Orders';
+import NotFound from '../pages/NotFound';
+import '../styles/global.css';
+
+const App = () => {
+	return (
+		<BrowserRouter>
+			<Layout>
+				<Routes>
+					<Route path='/' element={<Home />} />
+					<Route path='/login' element={<Login />} />
+					<Route path='/password-recovery' element={<RecoveryPassword />} />
+					<Route path='/send-email' element={<SendEmail />} />
+					<Route path='/new-password' element={<NewPassword />} />
+					<Route path='/account' element={<MyAccount />} />
+					<Route path='/signup' element={<CreateAccount />} />
+					<Route path='/checkout' element={<Checkout />} />
+					<Route path='/orders' element={<Orders />} />
+					<Route path='*' element={<NotFound />} />
+				</Routes>
+			</Layout>
+		</BrowserRouter>
+	);
+};
+
+export default App;
